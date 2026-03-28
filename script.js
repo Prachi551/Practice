ac602397506a990adaf6920a27afecc32d862b4b
Prachi <prachiaky53@gmail.com>
2026-03-28 15:04:24 +0530

    commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// const headingelement=document.getElementById("myheading");
+// headingelement.textContent="Welcome to JavaScript!";
+// headingelement.style.color="blue";
+// headingelement.style.backgroundColor="yellow";
+
+
+// const div=document.getElementById("mydiv");
+// div.style.width="1000px";
+// div.style.height="1000px";
+// div.style.backgroundColor="black"; 
+// const paragraph=document.querySelector("#content p");
+// paragraph.textContent="btech cse core"
+// paragraph.style.color="yellow";
+// const heading=document.querySelector("#content h1");
+// heading.textContent="welcome to query selector";
+// heading.style.color="orange";
+
+// const title = document.querySelector("title");
+// title.textContent = "Updated Title";
+
+// const selectedParagraphs = document.querySelectorAll("p.selected");
+
+// Loop through the selected elements and do something with them
+// selectedParagraphs.forEach(p => {
+//   console.log(p.textContent);
+// });
+// console.log("hello");
+
+// const newpara=document.createElement("p");
+// newpara.textContent=("This is a new para created by prachii");
+// const container=document.getElementById("container");
+// container.appendChild(newpara);
+
+
+// const div=document.getElementById("container");
+// console.log(div);
+// const div=document.querySelectorAll("#container");
+// console.log(div);
+
+const h2=document.querySelector("h2");
+h2.innerText="Welcome to JavaScript!";
+console.log(h2.innerText);
+
+
+
+
+
+
+
