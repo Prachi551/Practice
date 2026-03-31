b4589723a887c566d37f728a9282f9195ebf5ca4
Prachi <prachiaky53@gmail.com>
2026-03-31 19:52:19 +0530

    commit

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
-// const headingelement=document.getElementById("myheading");
-// headingelement.textContent="Welcome to JavaScript!";
-// headingelement.style.color="blue";
-// headingelement.style.backgroundColor="yellow";
+// javascript is dyanamically typed language. It means that you don't have to specify the data type of a variable when you declare it. The data type is determined at runtime based on the value assigned to the variable. For example:   
+// let x = 10; // x is a number
+// x = "Hello"; // x is now a string
+// console.log("hello world");
+// let a=67
+// console.log(a);
+// let 78a=90
+// console.log(78a); // this will give an error because variable names cannot start with a number
+// let _a=90
+// console.log(_a); // this is valid because variable names can start with an underscore
+// let $a=90
+// console.log($a); // this is valid because variable names can start with a dollar sign   
+
+// var let const in javascript:
+// var is globally scoped while let and const are block scoped.
+// var can be updated and redeclared within its scope
+// let can be updated but not redeclared within its scope
+// const can neither be updated nor redeclared within its scope. It must be initialized at the time of declaration.
+
+// let b ="hello"
+// let b=7
+// console.log(b); (gives error)
+
+// let b="hello"
+// let c=7
+// {
+//     let b=90
+//     console.log(b);
+
+// }
+// console.log(b);
+// var a=7
+// var a=90
+// console.log(a);
+
+// nn bb ss u primitives in javascript:
+// let a = null;
+// let b= 345;
+// let c= true;
+// let d=BigInt("567") + BigInt("123");
+// let e= "hello world";
+// let f= Symbol("I am a symbol");
+// let g= undefined;
+// console.log(a,b,c,d,e,f,g);
+
+// objects in javascript:(non primitive)
+// const item={
+//     "Harry":true,
+//     "Shubham":false,
+//     "Rohan":67
+// }
+// console.log(item["Harry"]);
 
 
-// const div=document.getElementById("mydiv");
-// div.style.width="1000px";
-// div.style.height="1000px";
-// div.style.backgroundColor="black"; 
-// const paragraph=document.querySelector("#content p");
-// paragraph.textContent="btech cse core"
-// paragraph.style.color="yellow";
-// const heading=document.querySelector("#content h1");
-// heading.textContent="welcome to query selector";
-// heading.style.color="orange";
-
-// const title = document.querySelector("title");
-// title.textContent = "Updated Title";
-
-// const selectedParagraphs = document.querySelectorAll("p.selected");
-
-// Loop through the selected elements and do something with them
-// selectedParagraphs.forEach(p => {
-//   console.log(p.textContent);
-// });
-// console.log("hello");
-
-// const newpara=document.createElement("p");
-// newpara.textContent=("This is a new para created by prachii");
-// const container=document.getElementById("container");
-// container.appendChild(newpara);
-
-
-// const div=document.getElementById("container");
-// console.log(div);
-// const div=document.querySelectorAll("#container");
-// console.log(div);
-
-const h2=document.querySelector("h2");
-h2.innerText="Welcome to JavaScript!";
-console.log(h2.innerText);
-
-
+let a="hello" 
+let b = 50 
+console.log(a+b);
+console.log(typeof(a));
 
 
 
